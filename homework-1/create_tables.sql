c6b2c0530f572523bc47837fbe2e46f794e7243f
Ivan <ivan.koltsov.1994@mail.com>
2023-04-09 15:20:19 +0500

    add create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+create table employees
+(
+  employee_id serial PRIMARY KEY,
+  first_name varchar(100) not NULL,
+  last_name varchar(100) NOT NULL,
+  title varchar(30) NOT NULL ,
+  birth_date date NOT NULL,
+  notes text NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(5) PRIMARY KEY NOT NULL,
+	company_name varchar(50) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id varchar(5) PRIMARY KEY NOT NULL,
+	customer_id varchar(5) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	city varchar(50) NOT NULL
+)
+
